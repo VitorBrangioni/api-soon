7cda38c387b3994d853bd429122ec6b3dce494ef
Vitor Brangioni <vitorh.brangioni@gmail.com>
2021-03-15 21:32:02 -0300

    Included new plan

--- src/api/plan/controller.js
+++ src/api/plan/controller.js
@@ -2,6 +2,7 @@ const plans = [
   { name: 'Plan A', id: '43274268472368472' },
   { name: 'Plan B', id: '47237847238472343' },
   { name: 'Plan C', id: '31231231231212315' },
+  { name: 'Plan D', id: '76476743675634777' },
 ];
 
 exports.show = (req, res) => {
